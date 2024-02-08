560a7609352fd951753b7ef1f513a7215fef9151
charbelnohra <nohracharbel113@gmail.com>
2024-02-08 17:42:24 +0200

    Install all packages

--- sentences.py
+++ sentences.py
@@ -1,151 +1,152 @@
 hi = ["hi",
-    "hello",
-    "good morning",
-    "good evening",
-    "good afternoon",
-    "hey"]
+      "hello",
+      "good morning",
+      "good evening",
+      "good afternoon",
+      "hey"]
 
 something = ["she sells seashells on the sea shore",
-            "a volcano blow up in Italy",
-            "there nothing to talk about",
-            "corona virus is very dangerous",
-            "how much wood would a woodchuck chuck if a woodchuck could chuck wood. as much wood as a woodchuck would a woodchuck chuck if a woodchuck could chuck wood",
-            "shut up the shutters and sit in the shop",
-            "a pessimistic pest exists amdist us",
-            "there are roughly 100 billion trillion stars in the universe, and you are brighter than them all",
-            "the Megalodon shark had around 276 teeth some of them were seven inches long",
-            "what do you call a train carrying bubblegum?, >> a chew-chew train",
-            "computers and mobiles were invented to save our time",
-            "why did the nurse need a red pen at work, >> in case she needed to draw blood",
-            "what do you call a boomerang that won't come back?, >> a stick",
-            "what's the best about switzerland, >> i don't know but the flag is a big plus",
-            "what breed of dog can jump higher than buildings, >> any dog, because buildings can not jump"]
+             "a volcano blow up in Italy",
+             "there nothing to talk about",
+             "corona virus is very dangerous",
+             "how much wood would a woodchuck chuck if a woodchuck could chuck wood. as much wood as a woodchuck "
+             "would a woodchuck chuck if a woodchuck could chuck wood",
+             "shut up the shutters and sit in the shop",
+             "a pessimistic pest exists amdist us",
+             "there are roughly 100 billion trillion stars in the universe, and you are brighter than them all",
+             "the Megalodon shark had around 276 teeth some of them were seven inches long",
+             "what do you call a train carrying bubblegum?, >> a chew-chew train",
+             "computers and mobiles were invented to save our time",
+             "why did the nurse need a red pen at work, >> in case she needed to draw blood",
+             "what do you call a boomerang that won't come back?, >> a stick",
+             "what's the best about switzerland, >> i don't know but the flag is a big plus",
+             "what breed of dog can jump higher than buildings, >> any dog, because buildings can not jump"]
 
 doing = ["i am washing the dishes",
-        "i am eating icecream",
-        "i am free now ask what you want",
-        "i am studying for my exams",
-        "i have some business to do",
-        "enjoying the moment with you",
-        "i am programming a chatbot",
-        "cleaning the house",
-        "ï want to sleep",
-        "i want to take a shower",
-        "watching a movie",
-        "playing drums",
-        "singing with my wonderful voice :)",
-        "i am doing sports right now so i can not talk to you"]
+         "i am eating icecream",
+         "i am free now ask what you want",
+         "i am studying for my exams",
+         "i have some business to do",
+         "enjoying the moment with you",
+         "i am programming a chatbot",
+         "cleaning the house",
+         "ï want to sleep",
+         "i want to take a shower",
+         "watching a movie",
+         "playing drums",
+         "singing with my wonderful voice :)",
+         "i am doing sports right now so i can not talk to you"]
 
 how = ["how you doin",
-      "how you doing",
-      "wassup",
-      "how are you",
-      "are you good"]
+       "how you doing",
+       "wassup",
+       "how are you",
+       "are you good"]
 
 happiness = ["be happy for this moment.this moment is your life",
-            "think happy, be happy",
-            "be healthy and take care of yourself, but be happy with the beautiful things that make you, you",
-            "the biggest adventure you can ever take is to live the life of your dreams",
-            "life is the dancer an you are the dance",
-            "the point is not to pay back kindness but to pass it on",
-            "no medicine cures what happiness cannot",
-            "the purpose of our lives is to be happy",
-            "being happy never goes out of style",
-            "happiness is the best makeup",
-            "if you want to be happy, be",
-            "independence is happiness",
-            "start each day with a grateful heart",
-            "i destroy my enemies when i make them my friends"]
+             "think happy, be happy",
+             "be healthy and take care of yourself, but be happy with the beautiful things that make you, you",
+             "the biggest adventure you can ever take is to live the life of your dreams",
+             "life is the dancer an you are the dance",
+             "the point is not to pay back kindness but to pass it on",
+             "no medicine cures what happiness cannot",
+             "the purpose of our lives is to be happy",
+             "being happy never goes out of style",
+             "happiness is the best makeup",
+             "if you want to be happy, be",
+             "independence is happiness",
+             "start each day with a grateful heart",
+             "i destroy my enemies when i make them my friends"]
 
 love = ["Don’t make friends who are comfortable to be with. Make friends who will force you to lever yourself up",
-       "The most beautiful discovery true friends make is that they can grow separately without growing apart",
-       "Happiness can be found even in the darkest of times, if one only remembers to turn on the light",
-       "They may forget what you said, but they will never forget how you made them feel",
-       "A real friend is one who walks in when the rest of the world walks out",
-       "Time does not take away from friendship, nor does separation",
-       "A single rose can be my garden…a single friend, my world",
-       "It’s the friends you can call up at 4 a.m. that matter"]
+        "The most beautiful discovery true friends make is that they can grow separately without growing apart",
+        "Happiness can be found even in the darkest of times, if one only remembers to turn on the light",
+        "They may forget what you said, but they will never forget how you made them feel",
+        "A real friend is one who walks in when the rest of the world walks out",
+        "Time does not take away from friendship, nor does separation",
+        "A single rose can be my garden…a single friend, my world",
+        "It’s the friends you can call up at 4 a.m. that matter"]
 
 bye = ["bye",
-      "goodnight",
-      "farewell",
-      "so long",
-      "see you",
-      "adios",
-      "au revoir"]
+       "goodnight",
+       "farewell",
+       "so long",
+       "see you",
+       "adios",
+       "au revoir"]
 
 thanks = ["thank you",
-         "thanks",
-         "gracias"]
+          "thanks",
+          "gracias"]
 
 welcome = ["welcome",
-          "my pleasure",
-          "no worries",
-          "you are welcome",
-          "no problem"]
+           "my pleasure",
+           "no worries",
+           "you are welcome",
+           "no problem"]
 
 play = ["let's go",
-       "let's play",
-       "come on",
-       "okay",
-       "go",
-       "let's begin",
-       "start",
-       "let's start"]
+        "let's play",
+        "come on",
+        "okay",
+        "go",
+        "let's begin",
+        "start",
+        "let's start"]
 
-wiki = ["what","who","when","where","why"]
+wiki = ["what", "who", "when", "where", "why"]
 
 fine = ["doing well thanks",
-       "i am fine thank you for asking",
-       "i am fine thanks",
-       "fine",
-       "i am fine",
-       "fine thank you",
-       "i'm good",
-       "good",
-       "very well",
-       "with you i am always good"]
+        "i am fine thank you for asking",
+        "i am fine thanks",
+        "fine",
+        "i am fine",
+        "fine thank you",
+        "i'm good",
+        "good",
+        "very well",
+        "with you i am always good"]
 
 work = ["what are you doing",
-       "are you busy",
-       "are you free",
-       "what are you working on"]
+        "are you busy",
+        "are you free",
+        "what are you working on"]
 
 some = ["something",
-       "story",
-       "jokes"]
+        "story",
+        "jokes"]
 
 sad = ["sad",
-      "bad",
-      "tired",
-      "not good"]
+       "bad",
+       "tired",
+       "not good"]
 
 friend = ["love",
-         "like"]
+          "like"]
 
 great = ["great",
-        "nice to hear this from you",
-        "very well",
-        "good"]
+         "nice to hear this from you",
+         "very well",
+         "good"]
 
 google = ["Google"]
 
 calcul = ["",
-         "select operation",
-         "1-Add",
-         "2-Subtract",
-         "3-Multiply",
-         "4-Divide",
-         ""]
+          "select operation",
+          "1-Add",
+          "2-Subtract",
+          "3-Multiply",
+          "4-Divide",
+          ""]
 
 research = ["search"]
 
 job = ["i can tell you jokes,or anything",
-      "go to google",
-      "wikipedia",
-      "google search",
-      "you can play with me",
-      "tell you the weather",
-      "tell you the date",
-      "you can ask for calculator",
-      "i can relax you"]
+       "go to google",
+       "wikipedia",
+       "google search",
+       "you can play with me",
+       "tell you the weather",
+       "tell you the date",
+       "you can ask for calculator",
+       "i can relax you"]
